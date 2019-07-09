8be2f730f3b7a3d1cb52085e38825dea44155c27
Your Name <2514366361@qq.com>
2019-07-09 20:33:43 +0800

    路由规则

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
   <div id="app">
-
+<!-- 一级路由对应组件渲染的位置 -->
+<router-view></router-view>
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'app',
-  components: {
+  name: 'app'
 
-  }
 }
 </script>
 
---
+++ src/views/login/index.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="login-container">
+    Login
+    <div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+
+</style>
