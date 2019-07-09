ff792cec5a2b8aad90f29245bc4ffa711760fef3
Your Name <2514366361@qq.com>
2019-07-09 20:49:05 +0800

    参考

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
 <template>
   <div id="app">
-<!-- 一级路由对应组件渲染的位置 -->
-<router-view></router-view>
+    <!-- 一级路由对应组件渲染的位置 -->
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'app'
-
 }
 </script>
 
-<style lang="less">
-
-</style>
+<style lang="less"></style>
